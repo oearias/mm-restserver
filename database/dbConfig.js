77ba69fad8d77c5599a05f5c00110ab7a22160cd
Your NameOscar <you@example.com>
2022-02-14 15:36:51 -0600

    Second commit

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    password: '1989023aB9.',
     database: 'mm',
     waitForConnections: true,
     connectionLimit: 10,
