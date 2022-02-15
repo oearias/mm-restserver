9d2485cc5de76cba988333948917158ac36a7394
Your NameOscar <you@example.com>
2022-02-15 15:52:07 -0600

    Se corrige

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ async function getConnection(){
     try {
         const pool = mysql.createPool({
             host: process.env.DB_HOST,
-            user: process.env.DB_USER,
+            user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
             waitForConnections: true,
